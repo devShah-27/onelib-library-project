ebcff67d3030ec19de7c9f826082a62486ce0591
Dev Shah <shahdev149@gmail.com>
2022-11-06 19:58:54 +0530

    add media queries

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ------------------------------navbar scroll effect----------------------------------------
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
@@ -7,4 +8,21 @@ window.onscroll = function () {
         document.getElementById("header").style.top = "-100px";
     }
     prevScrollpos = currentScrollPos;
+}
+
+// ----------------------------navbar open/close--------------------------------
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
 }
